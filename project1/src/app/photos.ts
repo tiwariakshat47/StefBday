39eac1422cc636558d452643b86b597f3a83c6d4
Akshat Tiwari <akki@Akshats-MacBook-Pro.local>
2025-06-28 18:00:24 -0700

    no idea whats happening

---
+++ project1/src/app/photos.ts
@@ -0,0 +1,26 @@
+export const photos = [
+    {
+        src: "/stef_2561.jpg",
+        className: "top-16 left-8 rotate-[-6deg]",
+    },
+    {
+        src: "/stef_6210.jpeg",
+        className: "top-[30%] right-10 rotate-[6deg]",
+    },
+    {
+        src: "/stef_7979.jpg",
+        className: "bottom-[20%] left-[6%] rotate-[12deg]",
+    },
+    {
+        src: "/stef_8104.png",
+        className: "bottom-[35%] right-[4%] rotate-[-10deg]",
+    },
+    {
+        src: "/stef_8458.jpg",
+        className: "top-[45%] left-[2%] rotate-[8deg]",
+    },
+    {
+        src: "/stef_8533.jpg",
+        className: "bottom-8 right-[6%] rotate-[5deg]",
+    },
+];
